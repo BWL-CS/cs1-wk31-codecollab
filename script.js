5becfb4966694bab5d20c8ab4643effcdb5724f2
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-04-14 15:05:28 +0000

    fixed load

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ describe how the code works using vocab like STATEMENT, VARIABLE, FUNCTION, etc.
 */
 
 // SECTION #1 
+const button1 = document.querySelector('#btn-1');
+button1.addEventListener('click', changeText);
+
 function changeText() {
   const element1 = document.querySelector('#element-1');
   element1.textContent = "Congratulations! You've changed this text using JS.";
 }
-const button1 = document.querySelector('#btn-1');
-button1.addEventListener('click', changeText);
 
   
 // SECTION #2
 const button2 = document.querySelector('#btn-2');
-const element2 = document.querySelector('#element-2');
-
-button2.onclick = changeStyle;
+button2.addEventListener('click', changeStyle);
 
 function changeStyle() {
+  const element2 = document.querySelector('#element-2');
   element2.style.color = "green";
   element2.style.fontSize = "20px";
 }
