88fe36a282dd88e4005cd040d5b5970addebb4ba
Katerina Walter <94501766+KaterinaWalter@users.noreply.github.com>
2025-04-29 11:15:11 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("hello world");
+const cup = document.querySelector("#cup-display");
+
+document.querySelector("#add-milk").addEventListener("click", function () {
+  cup.classList.add("milk");
+  cup.textContent = "Milk added!";
+});
+
+document.querySelector("#add-caramel").addEventListener("click", function () {
+  cup.classList.add("caramel");
+  cup.textContent = "Caramel syrup added!";
+});
+
+document.querySelector("#add-ice").addEventListener("click", function () {
+  const snowflake = document.createElement("div");
+  snowflake.textContent = "❄️ Ice added!";
+  cup.appendChild(snowflake);
+});
